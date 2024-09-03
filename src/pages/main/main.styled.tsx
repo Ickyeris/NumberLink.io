bbb98ce26ffd1b07a80777666658e7394a11fa3e
Ickyeris <david.serrano.ja@gmail.com>
2024-09-03 16:45:30 -0700

    1. Added folder structures, as well as a template 'grid' component

---
+++ src/pages/main/main.styled.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+
+
+
+export const OuterContainer = styled.div`
+    width: 100%;
+    height: 100vh;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+export const InnerContainer = styled.div`
+    height: 50%;
+    aspect-ratio: 1 / 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
---
+++ src/pages/main/main.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Grid from '../../components/grid/grid';
+import { InnerContainer, OuterContainer } from './main.styled';
+
+
+
+
+const MainPage = () => {
+    return (
+        <OuterContainer>
+            <h1>Work in Progress...</h1>
+            <InnerContainer>
+                <Grid/>
+            </InnerContainer>
+        </OuterContainer>
+    )
+}
+
+export default MainPage;
